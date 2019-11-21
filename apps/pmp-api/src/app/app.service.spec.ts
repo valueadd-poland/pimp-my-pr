ee3f5f24f844a42b4704c4ebfc1623effd263da8
Maciej Sikorski <maciej.sikorski@valueadd.pl>
2019-11-21 09:12:28 +0100

    ci(pmp): add CI from github actions
    add github actions ci configuration file
    fix tests to pass ci
    
    refs #1

--- apps/pmp-api/src/app/app.service.spec.ts
+++ apps/pmp-api/src/app/app.service.spec.ts
@@ -15,7 +15,7 @@ describe('AppService', () => {
 
   describe('getData', () => {
     it('should return "Welcome to api!"', () => {
-      expect(service.getData()).toEqual({ message: 'Welcome to api!' });
+      expect(service.getData()).toEqual('Welcome to api!');
     });
   });
 });
