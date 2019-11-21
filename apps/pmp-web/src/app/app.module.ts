457e42a6dbf2479185a312b2dae3b91ad2e9e154
ABartoszko <adam.bartoszko@valueadd.pl>
2019-11-21 10:29:10 +0100

    feat(pmp): add Angular Material
    resolve #27

--- apps/pmp-web/src/app/app.module.ts
+++ apps/pmp-web/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, NoopAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
