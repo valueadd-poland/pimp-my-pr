89c29ec76ff819129a334883b1e62d01f4807bec
Maciej Sikorski <maciej.sikorski@valueadd.pl>
2019-11-21 13:58:19 +0100

    ci(pmp): add @valueadd/dev to ci
    add valueadd-dev.json5
    add steps to validate code from ci
    
    close #1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Pimp My PR
+
 A brilliant tool that will pimp your pull requests by collecting and presenting PR statistics.
--- apps/pmp-api/src/app/app.controller.spec.ts
+++ apps/pmp-api/src/app/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
   describe('getData', () => {
     it('should return "Welcome to api!"', () => {
       const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Welcome to api!' });
+      expect(appController.getData()).toEqual('Welcome to api!');
     });
   });
 });
