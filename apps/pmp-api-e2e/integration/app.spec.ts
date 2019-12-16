7b38a88104264a293a9002755952685a46288785
Maciej Sikorski <maciej.sikorski@valueadd.pl>
2019-12-16 15:48:38 +0100

    feat(pmp-api): add pmp-api-e2e application
    close #40

---
+++ apps/pmp-api-e2e/integration/app.spec.ts
@@ -0,0 +1,29 @@
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+//  TODO to remove, disable only to show proposal of usage
+//  tslint:disable-next-line
+import { AppModule } from '../../pmp-api/src/app/app.module';
+
+describe('Application', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile();
+
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  it(`/GET hello`, () => {
+    return request(app.getHttpServer())
+      .get('/hello')
+      .expect(200, 'Welcome to api!');
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
---
+++ apps/pmp-api-e2e/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'pmp-api-e2e',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/pmp-api-e2e'
+};
