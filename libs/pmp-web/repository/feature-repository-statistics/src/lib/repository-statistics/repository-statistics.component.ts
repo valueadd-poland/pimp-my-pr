bacba2ada8de34e8f016be2a3f34946293a8a555
Arkadiusz Pałka <arkadiusz.palka@valueadd.pl>
2020-01-24 11:57:55 +0100

    fix(pmp-web): fix error while trying get properties of null
    object

--- libs/pmp-web/repository/feature-repository-statistics/src/lib/repository-statistics/repository-statistics.component.ts
+++ libs/pmp-web/repository/feature-repository-statistics/src/lib/repository-statistics/repository-statistics.component.ts
@@ -5,6 +5,28 @@ import { untilDestroyed } from 'ngx-take-until-destroy';
 import { RepositoryModel } from '@pimp-my-pr/shared/domain';
 import { first } from 'rxjs/operators';
 
+const mockedData = {
+  // TODO mocked data for repository
+  owner: 'valueadd',
+  fullName: 'pimp-my-pr',
+  name: 'pmp',
+  repositoryPictureUrl: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+  prsStatistics: [
+    {
+      author: 'asda',
+      authorAvatarUrl: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+      commentsCount: 123,
+      createdAt: '12/12/12',
+      linesOfCodeToCheck: 3245,
+      reviewCommentsCount: 234,
+      id: 2345,
+      timeWaiting: '124',
+      title: '12414',
+      url: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png'
+    }
+  ]
+};
+
 @Component({
   selector: 'pimp-my-pr-repository-statistics',
   templateUrl: './repository-statistics.component.html',
@@ -12,27 +34,7 @@ import { first } from 'rxjs/operators';
 })
 export class RepositoryStatisticsComponent implements OnDestroy, OnInit {
   repositoryName: string | null;
-  repository: RepositoryModel = {
-    // TODO mocked data for repository
-    owner: 'valueadd',
-    fullName: 'pimp-my-pr',
-    name: 'pmp',
-    repositoryPictureUrl: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
-    prsStatistics: [
-      {
-        author: 'asda',
-        authorAvatarUrl: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
-        commentsCount: 123,
-        createdAt: '12/12/12',
-        linesOfCodeToCheck: 3245,
-        reviewCommentsCount: 234,
-        id: 2345,
-        timeWaiting: '124',
-        title: '12414',
-        url: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png'
-      }
-    ]
-  };
+  repository: RepositoryModel;
 
   constructor(
     private cdr: ChangeDetectorRef,
@@ -43,14 +45,13 @@ export class RepositoryStatisticsComponent implements OnDestroy, OnInit {
   ngOnDestroy(): void {}
 
   ngOnInit(): void {
-    console.log('elo');
     this.initGetRepositoryStatistics();
     this.initSubscribeRepositoryStatistics();
   }
 
   private initSubscribeRepositoryStatistics(): void {
     this.facade.repositoryStatistics$.pipe(untilDestroyed(this)).subscribe(repository => {
-      this.repository = repository;
+      this.repository = mockedData; /* TODO repository instead mockedData */
       this.cdr.markForCheck();
     });
   }
