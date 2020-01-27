cf18d9e406a5d4ab288a70df0aca364cf778de63
Arkadiusz Pałka <arkadiusz.palka@valueadd.pl>
2020-01-27 11:00:40 +0100

    refactor(pmp-web): change queering data to async pipeline

--- libs/pmp-web/repository/feature-repository-statistics/src/lib/repository-statistics/repository-statistics.component.ts
+++ libs/pmp-web/repository/feature-repository-statistics/src/lib/repository-statistics/repository-statistics.component.ts
@@ -1,8 +1,6 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SingleRepositoryStatisticsFacade } from '@pimp-my-pr/pmp-web/repository/data-access-single-repository-statistics';
 import { ActivatedRoute } from '@angular/router';
-import { untilDestroyed } from 'ngx-take-until-destroy';
-import { RepositoryModel } from '@pimp-my-pr/shared/domain';
 import { first } from 'rxjs/operators';
 
 @Component({
@@ -12,26 +10,14 @@ import { first } from 'rxjs/operators';
 })
 export class RepositoryStatisticsComponent implements OnDestroy, OnInit {
   repositoryName: string | null;
-  repository: RepositoryModel;
+  repository$ = this.facade.repositoryStatistics$;
 
-  constructor(
-    private cdr: ChangeDetectorRef,
-    private route: ActivatedRoute,
-    private facade: SingleRepositoryStatisticsFacade
-  ) {}
+  constructor(private route: ActivatedRoute, private facade: SingleRepositoryStatisticsFacade) {}
 
   ngOnDestroy(): void {}
 
   ngOnInit(): void {
     this.initGetRepositoryStatistics();
-    this.initSubscribeRepositoryStatistics();
-  }
-
-  private initSubscribeRepositoryStatistics(): void {
-    this.facade.repositoryStatistics$.pipe(untilDestroyed(this)).subscribe(repository => {
-      this.repository = repository;
-      this.cdr.markForCheck();
-    });
   }
 
   private initGetRepositoryStatistics(): void {
