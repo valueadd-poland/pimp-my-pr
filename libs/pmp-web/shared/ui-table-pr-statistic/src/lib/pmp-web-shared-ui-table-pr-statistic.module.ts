2c805393c34d16d213f14c53a76cff39faf47f5c
Arkadiusz Pałka <arkadiusz.palka@valueadd.pl>
2020-01-31 14:43:30 +0100

    feat(pmp-web): add mat sorting at table pr statistics

--- libs/pmp-web/shared/ui-table-pr-statistic/src/lib/pmp-web-shared-ui-table-pr-statistic.module.ts
+++ libs/pmp-web/shared/ui-table-pr-statistic/src/lib/pmp-web-shared-ui-table-pr-statistic.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TablePrStatisticComponent } from './table-pr-statistic/table-pr-statistic.component';
-import { MatIconModule, MatTableModule } from '@angular/material';
+import { MatIconModule, MatSortModule, MatTableModule } from '@angular/material';
 import { PmpWebSharedUiPictureLabelModule } from '@pimp-my-pr/pmp-web/shared/ui-picture-label';
 
 @NgModule({
-  imports: [CommonModule, MatTableModule, MatIconModule, PmpWebSharedUiPictureLabelModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatIconModule,
+    PmpWebSharedUiPictureLabelModule,
+    MatSortModule
+  ],
   exports: [TablePrStatisticComponent],
   declarations: [TablePrStatisticComponent]
 })
