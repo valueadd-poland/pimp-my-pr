62e9eb44bc3a582caf0556efd93e76a611c7457b
Arkadiusz Pałka <arkadiusz.palka@valueadd.pl>
2020-02-03 07:44:54 +0100

    feat(pmp-web): add table pr statistics presenter

---
+++ libs/pmp-web/shared/ui-table-pr-statistic/src/lib/table-pr-statistic/table-pr-statistics.presenter.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { PrStatistics } from '@pimp-my-pr/shared/domain';
+
+@Injectable()
+export class TablePrStatisticsPresenter {
+  public sortData(item: PrStatistics, property: string): string | number {
+    switch (property) {
+      case 'author':
+        return item.author.name;
+      case 'comments':
+        return item.commentsCount + item.reviewCommentsCount;
+      case 'reviewers':
+        return item.reviewers.length;
+      default:
+        return item[property];
+    }
+  }
+}
