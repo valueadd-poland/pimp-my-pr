9fe568b590231acb45d3c92a1ffe05c40118ac63
Wojciech Janaszek <trash.wjanaszek@gmail.com>
2020-02-04 15:58:04 +0100

    feat(pmp-web): add loading indicators to all views
    Add @ngneat/content-loader library to show
    facebook-like loading placeholders.
    
    Resolves #96

--- libs/pmp-web/shared/ui-header-container/src/lib/header-container/header-container.component.ts
+++ libs/pmp-web/shared/ui-header-container/src/lib/header-container/header-container.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'pmp-header-container',
   templateUrl: './header-container.component.html',
-  styleUrls: ['./header-container.component.scss']
+  styleUrls: ['./header-container.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderContainerComponent {
   @Input()
