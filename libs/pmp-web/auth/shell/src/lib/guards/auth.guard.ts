9de9df3eb5c568253da7e39e65cd691282ce39dc
Maciej Sikorski <maciejsikorski00@gmail.com>
2020-04-21 08:06:52 +0200

    fix(pmp-web): blinking login page

--- libs/pmp-web/auth/shell/src/lib/guards/auth.guard.ts
+++ libs/pmp-web/auth/shell/src/lib/guards/auth.guard.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthFacade } from '@pimp-my-pr/pmp-web/auth/data-access';
-import { Observable, of } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 import { first } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authFacade: AuthFacade) {}
+  constructor(private authFacade: AuthFacade, private router: Router) {}
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     if (!!next.queryParams['code'] && !!next.queryParams['platform']) {
@@ -16,6 +16,9 @@ export class AuthGuard implements CanActivate {
       const platform = next.queryParams['platform'];
       return this.authFacade.login(authCode, platform).pipe(first());
     } else {
+      if (!!this.authFacade.getSavedToken()) {
+        return from(this.router.navigate(['/user']));
+      }
       return of(true);
     }
   }
