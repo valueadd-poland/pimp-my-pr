19a30d75725d98db17bac168e2e173c179e6fe02
ABartoszko <adam.bartoszko@valueadd.pl>
2020-04-22 15:53:34 +0200

    feat(pmp): logout on unauthorized request
    resolve #182

--- libs/pmp-web/auth/shell/src/lib/interceptors/auth-interceptor.ts
+++ libs/pmp-web/auth/shell/src/lib/interceptors/auth-interceptor.ts
@@ -1,8 +1,15 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthFacade } from '@pimp-my-pr/pmp-web/auth/data-access';
 import { Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { catchError, filter } from 'rxjs/operators';
+import { HttpStatusCodes } from '@pimp-my-pr/shared/domain';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -13,15 +20,24 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(
-      !!this.authToken
-        ? req.clone({
-            setHeaders: {
-              Authorization: `Bearer ${this.authToken}`
-            }
-          })
-        : req.clone()
-    );
+    return next
+      .handle(
+        !!this.authToken
+          ? req.clone({
+              setHeaders: {
+                Authorization: `Bearer ${this.authToken}`
+              }
+            })
+          : req.clone()
+      )
+      .pipe(
+        catchError((err: HttpErrorResponse) => {
+          if (err.status === HttpStatusCodes.Unauthorized) {
+            this.authFacade.logout();
+          }
+          throw err;
+        })
+      );
   }
 
   private initAuthToken(): void {
