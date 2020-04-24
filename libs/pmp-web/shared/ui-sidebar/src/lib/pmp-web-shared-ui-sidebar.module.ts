52be63e289090d13c9febf03b5ed6beacc44cdd6
Maciej Sikorski <maciejsikorski00@gmail.com>
2020-04-24 17:01:15 +0200

    fix(pmp-web): add tooltips to the navigation bar items
    fixes #178

--- libs/pmp-web/shared/ui-sidebar/src/lib/pmp-web-shared-ui-sidebar.module.ts
+++ libs/pmp-web/shared/ui-sidebar/src/lib/pmp-web-shared-ui-sidebar.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { MatIconModule, MatSidenavModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  imports: [CommonModule, MatIconModule, RouterModule, MatSidenavModule],
+  imports: [MatTooltipModule, CommonModule, MatIconModule, RouterModule, MatSidenavModule],
   declarations: [SidebarComponent],
   exports: [SidebarComponent]
 })
