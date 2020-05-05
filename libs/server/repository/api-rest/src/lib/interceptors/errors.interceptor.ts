078f03afbc1e91781bce30c64cd15e8d80388a74
Maciej Sikorski <maciejsikorski00@gmail.com>
2020-05-05 12:29:59 +0200

    fix(api-user): handle user not found on user info
    Return Unauthorized if user is not found on /user-info.
    Change available scopes.

--- libs/server/repository/api-rest/src/lib/interceptors/errors.interceptor.ts
+++ libs/server/repository/api-rest/src/lib/interceptors/errors.interceptor.ts
@@ -12,13 +12,14 @@ import {
 } from '@pimp-my-pr/server/shared/domain';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError';
 
 @Injectable()
 export class ErrorsInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError(err => {
-        if (err instanceof CoreNotFoundException) {
+        if (err instanceof CoreNotFoundException || err instanceof EntityNotFoundError) {
           return throwError(new NotFoundException(err.message));
         }
         if (err instanceof CoreUnauthorizedFoundException) {
